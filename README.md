3320d068cdac148afbf34cd9ea9d7253b394e5b8
Andrey Samoylov <Drenjy@mail.ru>
2017-12-22 23:20:16 +0300

    завершающий коммит.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/Drenjy/play-with-travis.svg?branch=master)](https://travis-ci.org/Drenjy/play-with-travis)
 ### Play with Travis CI
 Add 1 commit. Test
--- test.py
+++ test.py
@@ -4,6 +4,16 @@ class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
         self.assertEqual(1 + 1, 2)
+		
+	def bed_test(foo):
+	foo.assertTrue(1==2)
+	
+	def test_split(self):
+      s = 'hello world'
+      self.assertEqual(s.split(), ['hello', 'world'])
+      # Проверим, что s.split не работает, если разделитель - не строка
+      with self.assertRaises(TypeError):
+          s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
